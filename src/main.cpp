c30d36004a99be343deebd0d03999fdb42021e99
SzamockiP <szamocki.piotr04@gmail.com>
2026-06-28 00:53:15 +0200

    engine start and two threads running

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
 #include <print>
+#include <atomic>
+#include <thread>
+#include <chrono>
 
 namespace py = pybind11;
 
 
 class Engine {
     public:
-    Engine() {
-        std::print("Engine created");
+    Engine() 
+    {
+
     }
 
-    void init() {
-        std::print("Engine initiated");
+    void init() 
+    {
+
     }
+
+    void run(py::function function)
+    {
+        _is_running.store(true, std::memory_order_relaxed);
+
+        py::gil_scoped_release release;
+
+        std::jthread logic_thread([this, function]()
+            {
+                py::gil_scoped_acquire acquire;
+
+                try
+                {
+                    function();
+                }
+                catch (const std::exception& e)
+                {
+                    std::print("[Error]: {}\n", e.what());
+                    stop();
+                }
+            });
+
+        while (_is_running.load(std::memory_order_relaxed))
+        {
+            std::print("[Running]");
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+
+    bool running() const
+    {
+        return _is_running.load(std::memory_order_relaxed);
+    }
+
+    void stop()
+    {
+        _is_running.store(false, std::memory_order_relaxed);
+    }
+
+private:
+    std::atomic<bool> _is_running{false};
 };
 
 PYBIND11_MODULE(lumapy, m){
@@ -20,5 +67,8 @@ PYBIND11_MODULE(lumapy, m){
 
     py::class_<Engine>(m, "Engine")
         .def(py::init<>())
-        .def("init", &Engine::init);
+        .def("init", &Engine::init)
+        .def("run", &Engine::run)
+        .def("running", &Engine::running)
+        .def("stop", &Engine::stop);
 }
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import lumapy
+import time
 
 eng = lumapy.Engine()
 
 eng.init()
+
+def fun():
+    for x in range(10):
+        time.sleep(1)
+        print("Dziala", x)
+    eng.stop()
+
+eng.run(fun)
+print("koniec")
