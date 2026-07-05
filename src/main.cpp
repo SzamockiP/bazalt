6568d3d2d72076dd78a2332b5a28d36cfb234b64
SzamockiP <szamocki.piotr04@gmail.com>
2026-07-05 16:24:08 +0200

    removed create_empty_buffer

--- src/main.cpp
+++ src/main.cpp
@@ -163,16 +163,6 @@ public:
         }
     }
 
-    py::object create_empty_buffer(size_t size_bytes, BufferType type) {
-        std::expected<std::shared_ptr<Buffer>, std::string> res = Buffer::create(*renderer_, nullptr, size_bytes, type);
-        if (res) {
-            return py::cast(res.value());
-        } else {
-            logger_.log(res.error());
-            throw std::runtime_error(res.error());
-        }
-    }
-
     py::object create_command_buffer() {
         auto res = CommandBuffer::create(*renderer_);
         if (res) {
@@ -316,7 +306,6 @@ PYBIND11_MODULE(lumapy, m) {
         .def("getMouseState", &Engine::get_mouse_state)
         .def("isKeyPressed", &Engine::is_key_pressed)
         .def("createBuffer", &Engine::create_buffer)
-        .def("createEmptyBuffer", &Engine::create_empty_buffer)
         .def("createCommandBuffer", &Engine::create_command_buffer)
         .def("createPipeline", &Engine::create_pipeline)
         .def("compileShader", &Engine::compile_shader)
